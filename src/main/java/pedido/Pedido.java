656607a7a101ed6b3f8f3f7c1baf434a892f1646
Emerson-feral <emerson_feral@hotmail.com>
2022-07-18 09:41:16 -0300

    Functionalities added

--- src/main/java/pedido/Pedido.java
+++ src/main/java/pedido/Pedido.java
@@ -1,6 +1,9 @@
 package pedido;
 
+import ingredientes.Ingrediente;
+
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class Pedido{
 
@@ -28,22 +31,67 @@ public class Pedido{
 
     public double calcularTotal(Cardapio cardapio){
         double total= 0;
-        //TODO
+
+        for (var item:itens) {
+            double preco =0.0;
+            var shake = item.getShake();
+            var tamanho = shake.getTipoTamanho().toString();
+            var quantidade = item.getQuantidade();
+            var base = cardapio.buscarPreco(shake.getBase());
+
+            if (tamanho == "M") {
+                preco = base * 1.3;
+            }else if (tamanho == "G") {
+                preco = base * 1.5;
+            }else{
+                preco = base;
+            }
+
+            if(shake.getAdicionais() != null){
+                for (var adicional : shake.getAdicionais()) {
+                    var resultado = cardapio.buscarPreco((Ingrediente) adicional);
+                    preco += resultado;
+
+                }
+            }
+            total += quantidade * preco;
+        }
+
         return total;
     }
 
     public void adicionarItemPedido(ItemPedido itemPedidoAdicionado){
-        //TODO
+
+        var itemPedidoEncontrado = itens.stream()
+                .filter(item -> item.getShake().equals(itemPedidoAdicionado.getShake()))
+                .findFirst()
+                .orElse(null);
+
+        if (itemPedidoEncontrado != null) {
+            itemPedidoEncontrado.addQuantidade(itemPedidoAdicionado.getQuantidade());
+        } else {
+            itens.add(itemPedidoAdicionado);
+        }
     }
 
     public boolean removeItemPedido(ItemPedido itemPedidoRemovido) {
-        //substitua o true por uma condição
-        if (true) {
-            //TODO
+
+        var itemPedidoEncontrado = itens.stream()
+                .filter(item -> item.getShake().equals(itemPedidoRemovido.getShake()))
+                .findFirst()
+                .orElse(null);
+
+
+        if (itemPedidoEncontrado != null) {
+            itemPedidoEncontrado.subtrairQuantidade(1);
+            if(itemPedidoEncontrado.getQuantidade() <= 0){
+                itens.remove(itemPedidoEncontrado);
+            }
+            return true;
         } else {
             throw new IllegalArgumentException("Item nao existe no pedido.");
         }
-        return false;
+
     }
 
     @Override
