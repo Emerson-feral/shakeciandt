dbd25f21050b1ccb566dbec0c5d3091dd475d7ae
Emerson-feral <emerson_feral@hotmail.com>
2022-07-18 09:33:02 -0300

    CompareTo fixed

--- src/main/java/ingredientes/Base.java
+++ src/main/java/ingredientes/Base.java
@@ -26,7 +26,7 @@ public class Base implements Ingrediente,Comparable<Ingrediente>{
     //É necessário consertar o compareTo, para imprimir na ordem correta.
     @Override
     public int compareTo(Ingrediente ingrediente) {
-        return ingrediente.obterTipo().toString().compareToIgnoreCase(this.obterTipo().toString());
+        return this.obterTipo().toString().compareTo(ingrediente.obterTipo().toString());
     }
 
     @Override
@@ -43,4 +43,5 @@ public class Base implements Ingrediente,Comparable<Ingrediente>{
     public Enum obterTipo() {
         return this.tipoBase;
     }
+
 }
